371ff263ee017f90a1394bd86e39f3c2c74b43fb
Timm Bäder <mail@baedert.org>
2016-10-01 10:00:30 +0200

    utils: Fix parsing of empty objects

--- src/gtkutils.d
+++ src/gtkutils.d
@@ -14,11 +14,13 @@ string generate_ui(const string ui_data) {
 }
 
 
-string __generate_ui(const string ui_data, bool just_members = false) {
+private string __generate_ui(const string ui_data, bool just_members = false) {
+	immutable char EOF = cast(char)255;
 	string result;
 	auto input = ui_data.strip();
 	int pos = 0;
 	char c = input[0];
+	string ident = null;
 	string[string] object_ids;
 
 	uint irrelevant_id = 0;
@@ -28,63 +30,77 @@ string __generate_ui(const string ui_data, bool just_members = false) {
 
 	pure
 	bool separates(char c) {
-		return ['{', '}', '=', ' ', '\n', '\t'].canFind(c);
+		return ['{', '}', '=', ' ', '\n', '\t', '|'].canFind(c);
+	}
+
+	pure void advance() {
+		if (pos < input.length - 1) {
+			pos ++;
+			c = input[pos];
+		} else {
+			c = EOF;
+		}
+	}
+
+	pure
+	void skip_whitespace() {
+		while (c == ' ' || c == '\t' || c == '\n') {
+			advance();
+		}
 	}
 
 	//pure
 	void next() {
-		do {
-			if (pos < input.length - 1) {
-				pos ++;
-				c = input[pos];
-			} else {
-				c = '&';//-1;
-				break;
-			}
-		} while (c == ' ' || c == '\t' || c == '\n'); // Not \n!
-
-		if (!__ctfe) writeln ("c now: ", c != '\n' ? c : '_');
-	}
-
-	//pure
-	void skip_whitespace() {
-		while (c == ' ' || c == '\t' || c == '\n')
-			next();
-	}
-
-	pure
-	string get_ident() {
-		assert(!separates(c));
-		string ident;
-		while (!separates(c)) {
-			ident ~= c;
-			// Don't use next() here since that skips whitespace
-			pos ++;
-			c = input[pos];
+		//writeln ("Pos: ", pos, " Length: ", input.length);
+		if (pos >= input.length) {
+			c = EOF;
+			ident = [EOF];
+			return;
 		}
-		return ident;
+
+		skip_whitespace();
+		//writeln ("Starting with c ", c, ", pos ", pos, " and ident '", ident, "'");
+		ident = "";
+		int i = 0;
+		do {
+			ident ~= c;
+			advance();
+			i ++;
+		}
+		while (!separates(c) && pos < input.length - 1 );
+
+		skip_whitespace();
+		//if (!__ctfe) { writeln ("ident now: ", ident);
+		  //writeln ("c now: ", c != EOF ? to!string(c) : "eof");
+		//}
+	}
+
+	void expect(string e) {
+		if (ident != e) {
+			if (!__ctfe)
+				writeln ("Expected ", e, ", but found ", ident, " at pos ", pos);
+			assert(ident == e);
+		}
 	}
 
 	pure
 	string get_until(char _c) {
 		string result;
-		while (c != _c) {
+		while (c != _c && c != EOF) {
 			result ~= c;
-			pos ++;
-			c = input[pos];
+			advance();
 		}
 		return result;
 	}
 
 	string parse_object (bool toplevel = false) {
-		skip_whitespace();
-		string object_type = get_ident();
-		if (!__ctfe) writeln("Object: ", object_type);
+		//skip_whitespace();
+		string object_type = ident;
 		string object_id = null;
 		next();
-		if (c != '{') {
+		if (ident != "{") {
 			// Read object ID (optional!)
-			object_id = get_ident();
+			object_id = ident;
 			next();
 			if (!toplevel)
 				object_ids[object_id] = object_type;
@@ -97,25 +113,37 @@ string __generate_ui(const string ui_data, bool just_members = false) {
 
 		string[string] construct_props;
 		string[string] non_construct_props;
-		assert(c == '{');
-		next(); // skip newline
-		next(); // skip to beginning of line
-		while (c == '.' || c == '|') {
-			bool construct = (c == '|');
-			// Parse property
+		expect("{");
+		//assert(ident == "{");
+		//next();
+		//assert(ident != "{");
+		//writeln("");
+		//writeln("");
+		//writeln("");
+		next();
+		//writeln ("Ident before starting to parse props: ", ident, ", c: ", c);
+		while (ident[0] == '.' || ident[0] == '|') {
+		//while (ident == "." || ident == "|") {
+		//while (c == '.' || c == '|') {
+			bool construct = (ident[0] == '|');
+			//next();
+			string prop_name = ident;
+			//writeln ("prop name; ", prop_name, "(", construct, ")");
 			next();
-			string prop_name = get_ident();
-			next();
-			assert(c == '=');
-			next();
-			string prop_value = get_until('\n');
+			expect("=");
+			//next();
+			string prop_value = get_until('\n').strip();
+			//writeln ("Value: ", prop_value);
 			if (construct) {
-				construct_props[prop_name] = prop_value;
+				construct_props[prop_name[1..$]] = prop_value;
 			} else {
-				non_construct_props[prop_name] = prop_value;
+				non_construct_props[prop_name[1..$]] = prop_value;
 			}
+
 			next();
+			//writeln ("## After reading prop: ident=", ident, ", c=", c);
 		}
+
 
 		if (!toplevel) {
 			// Generate the declaration
@@ -140,33 +168,47 @@ string __generate_ui(const string ui_data, bool just_members = false) {
 			result ~= ");\n";
 		}
 
+		//if (!__ctfe)
+		//writeln ("before reading child objects: ", ident, ", c: ", c);
 		// Now read child objects
-		if (!__ctfe) writeln ("c before reading objects: ", c);
-		while (c != '}') { // until this object ends
+		//if (!__ctfe) writeln ("c before reading objects: ", c);
+		while (ident != "}") { // until this object ends
+		  //if (!__ctfe)
+			//writeln ("reading child object ...");
 			string child = parse_object();
 			result ~= object_id ~ ".add(" ~ child ~ ");\n";
 		}
 
-		next();
+		//next();
 		next();
 
 		return object_id;
 	}
 
+	// Kick off
+	next();
 	// Parse toplevel obj
 	parse_object(true);
-
 
 	// We simply do both things in both cases...
 	if (just_members) {
 		string member_result;
 		foreach (m; object_ids.keys) {
-			//if (m != "this")
+			if (m != "this")
 				member_result ~= object_ids[m] ~ " " ~ m ~ ";\n";
 		}
 		return member_result;
-	} else {
-		return result;
 	}
-	assert(0);
+
+	return result;
+}
+
+unittest {
+	string s = __generate_ui("Box this{}");
+	//writeln(s);
+	assert(s.strip == "super();"); // empty object, no properties, ...
+
+	string s2 = __generate_ui("Box this{|spacing = 12\n|foo = \"bla\"\n}");
+	//writeln(s2);
+	assert(s2.strip() == "super(12, \"bla\", );");
 }
