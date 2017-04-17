331e0b67ce4c55b4e26f84794d90b036c2edb085
Timm Bäder <mail@baedert.org>
2017-04-17 08:36:23 +0200

    import app css

--- src/app.d
+++ src/app.d
@@ -11,8 +11,6 @@ import std.algorithm;
 import mainwindow;
 import account;
 
-static immutable string app_css = import("style.css");
-
 class Corebird : GtkApplication.Application {
 public:
 	this() {
@@ -37,10 +35,10 @@ private:
 
 	void startup(GApplication.Application app) {
 		auto provider = new CssProvider();
-		provider.loadFromData(app_css);
+		provider.loadFromData(appCss);
 		StyleContext.addProviderForScreen(Screen.getDefault(),
-										  provider,
-										  600); // PRIORITY_APPLICATION
+		                                  provider,
+		                                  600); // PRIORITY_APPLICATION
 	}
 
 	void activate(GApplication.Application app) {
@@ -49,3 +47,50 @@ private:
 		window.showAll();
 	}
 }
+
+enum appCss = q{
+  @define-color topbar_bg #333;
+
+  .account-button {
+	  background-image: none;
+	  background-color: blue;
+  }
+
+  .topbar button image {
+	-gtk-icon-shadow: none;
+	color: #ddd;
+  }
+
+  .topbar button {
+	border: none;
+	box-shadow: none;
+	background-image: none;
+	border-radius: 0px;
+	background-color: @topbar_bg;
+	outline-color: #999;
+	margin: 0px;
+	-GtkWidget-window-dragging: 1;
+  }
+
+  .topbar button:hover {
+	background-color: shade(@topbar_bg, 1.3);
+  }
+
+
+  .topbar button:checked {
+	background-color: shade(@topbar_bg, 1.5);
+  }
+
+  .topbar button .badge {
+	background-color: shade(blue, 1.4);
+	background-image: none;
+	text-shadow: 0px 1px 1px #FFF;
+	border-radius: 20px;
+	border: 1px solid white;
+	padding: 3px 2px;
+  }
+
+  .topbar button .badge:backdrop {
+	background-color: grey;
+  }
+};
